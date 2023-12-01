765b64bf1e8be2094128a1e7175b4e7a06422de1
biruytskovsky <david.biruykov.02@mail.ru>
2023-12-02 00:21:33 +0200

    Добавьте файлы проекта.

---
+++ zadacha 3/zadacha 3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int x, i;
+    cout << "write num x (0 < x < 10^9): ";
+    cin >> x;
+    cout << "wtite num bit i: ";
+    cin >> i;
+
+    // Создание битовой маски с i-ым битом установленным в 0
+    int mask = ~(1 << i);
+
+    // Установка i-ого бита в 0
+    int result = x & mask;
+
+    cout << "num " << x << " c " << i << "i bit in 0: " << result << endl;
+
+    return 0;
+}
